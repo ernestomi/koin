ec98a00276a042db5a0ccd7550d5686481a2f930
Ernesto Monroy <ehmizmg@gmail.com>
2022-02-17 16:56:30 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='koin',
-    version='0.0.1',
+    version='0.0.2',
     description='A simple library for Coinbase Pro',
     long_description=long_description,
     long_description_content_type="text/markdown",
